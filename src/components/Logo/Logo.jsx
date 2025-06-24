a00306f0a6cbaadd10bdd5635e2d0be2f7749e8e
Elnq01 <qele95@gmail.com>
2025-06-24 05:56:09 -0700

    logo adjustment

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,6 @@
-// import LogoSrc from "../../../public/images/logo.svg"
+import LogoSrc from "../../../public/images/logo.svg"
 
 export default function Logo(){
-    return <img src="../../../public/images/logo.svg" alt="Logo" />
+    return <img src={LogoSrc} alt="Logo" />
+    // return <img src="../../../public/images/logo.svg" alt="Logo" />
 }
