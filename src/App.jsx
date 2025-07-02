1d5c0b74b69d7aa074b3bf46d4e0014af2d280dc
Olly-writes <modebemarieo@gmail.com>
2025-07-02 18:56:24 +0100

    fix: adjust welcome message positioning and logo styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { Routes, Route, NavLink } from 'react-router';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import SignInPage from './pages/SignInPage/SignInPage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
 import DashboardPage from './pages/DashboardPage/DashboardPage';
+import JournalEntryPage from './pages/JournalEntryPage/JournalEntryPage';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-      
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/journal" element={<JournalEntryPage />} />
+        </Routes>
 
-      {/* You can comment out if it is hinder  */}
-      <div style={{position:'fixed', bottom:'20px', left:'10px', display:'flex', gap:'20px'}}>
-        <NavLink to="/signin">Sign In Page</NavLink>
-        <NavLink to="/signup">Sign Up Page</NavLink>
-        <NavLink to="/dashboard">Dashboard Page</NavLink>
-      </div>
+    
+        <div style={{ position: 'fixed', bottom: '20px', left: '10px', display: 'flex', gap: '20px' }}>
+          <NavLink to="/signin">Sign In Page</NavLink>
+          <NavLink to="/signup">Sign Up Page</NavLink>
+          <NavLink to="/dashboard">Dashboard Page</NavLink>
+          <NavLink to="/journal">Journal Entry Page</NavLink>
+        </div>
     </>
   );
 }
