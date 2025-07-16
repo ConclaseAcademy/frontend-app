151576bdaf1f9a34e43c8566585a16e4f277d146
Elnq01 <qele95@gmail.com>
2025-07-16 04:52:25 -0700

    testing the api endpoint for signup

--- src/pages/SignUpPage/SignUpPageFormData.js
+++ src/pages/SignUpPage/SignUpPageFormData.js
@@ -3,24 +3,28 @@ export const formInputControlData = [
         id:0,
         label:"name",
         type:"text",
-        placeholder:"Enter your name"
+        placeholder:"Enter your name",
+        name:'name'
     },
     {
         id:1,
         label:"Email Address",
         type:"email",
-        placeholder:"Enter your email"
+        placeholder:"Enter your email",
+        name:'email'
     },
     {
         id:2,
         label:"Password",
         type:"password",
-        placeholder:"Enter your password"
+        placeholder:"Enter your password",
+        name:'password'
     },
     {
         id:3,
         label:"I agree to all terms and privacy conditions",
         type:"checkbox",
+        name:"switch"
         // placeholder:"Forgot password?"
     }
 ]
